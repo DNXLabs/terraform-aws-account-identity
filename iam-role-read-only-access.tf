f9b93e8cda7a27ce57ad617007ead1e8177a9108
Yifan Fu <yifan.fu@creditorwatch.com.au>
2022-10-22 01:44:21 +1100

    feat: Add ReadOnlyAccess IAM role

---
+++ iam-role-read-only-access.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_role" "read_only" {
+  name                 = "ReadOnlyAccess"
+  assume_role_policy   = data.aws_iam_policy_document.assume_role_saml.json
+  max_session_duration = var.role_max_session_duration
+}
+
+resource "aws_iam_role_policy_attachment" "read_only" {
+  role       = aws_iam_role.read_only.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
